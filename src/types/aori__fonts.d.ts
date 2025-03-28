038c9b1a0574f2614a1418f5909e8693913fc163
Andrew Duliba <a_duliba@protonmail.com>
2025-03-28 17:46:39 -0400

    1.0.4

---
+++ src/types/aori__fonts.d.ts
@@ -0,0 +1,15 @@
+declare module '@aori/fonts/font/sans' {
+  export const AoriSans: {
+    style: {
+      fontFamily: string;
+    };
+  };
+}
+
+declare module '@aori/fonts/font/mono' {
+  export const AoriMono: {
+    style: {
+      fontFamily: string;
+    };
+  };
+}
